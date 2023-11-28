9b4a5fed3c0758d747fce28e0f56613c0caca67f
thibauult <thibault.pensec@symphony.com>
2023-11-28 11:52:02 +0100

    Reverted code generation input to URL instead of local file

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate oapi-codegen --package=openhue -generate=client,types -o ./openhue/openhue.gen.go /Users/thibault.pensec/local/perso/openhue-api/build/openhue.yaml
+//go:generate oapi-codegen --package=openhue -generate=client,types -o ./openhue/openhue.gen.go https://api.redocly.com/registry/bundle/openhue/openhue/v2/openapi.yaml?branch=main
 
 import (
 	"openhue-cli/cmd"
