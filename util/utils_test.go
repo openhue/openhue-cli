e0ccc7a6b5c98d68c23ba97d74734d567f95f109
thibauult <thibault.pensec@symphony.com>
2023-11-28 22:37:05 +0100

    Fixed unit tests

--- util/utils_test.go
+++ util/utils_test.go
@@ -1,6 +1,11 @@
 package util
 
-import "testing"
+import (
+	"openhue-cli/openhue"
+	"openhue-cli/openhue/test"
+	"strings"
+	"testing"
+)
 
 type Light struct {
 	id   string
@@ -15,5 +20,14 @@ func TestPrintTable(t *testing.T) {
 
 	lights := []Light{{id: "1234", name: "Light 1"}, {id: "4321", name: "Light 2"}}
 
-	PrintTable(lights, PrintLightLineProcessor, "id", "name")
+	streams, _, out, _ := openhue.NewTestIOStreams()
+
+	PrintTable(streams, lights, PrintLightLineProcessor, "id", "name")
+
+	lines := strings.Split(out.String(), "\n")
+
+	test.AssertThatLineEqualsTo(t, lines, 0, "id     name   ")
+	test.AssertThatLineEqualsTo(t, lines, 1, "----   ----   ")
+	test.AssertThatLineEqualsTo(t, lines, 2, "1234   Light 1")
+	test.AssertThatLineEqualsTo(t, lines, 3, "4321   Light 2")
 }
