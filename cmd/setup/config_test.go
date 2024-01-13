1c857e412d597332901eb939e8f3386347d14283
thibauult <thibault.pensec@symphony.com>
2024-01-13 19:00:20 +0100

    Fixed unit tests

--- cmd/setup/config_test.go
+++ cmd/setup/config_test.go
@@ -1,13 +1,14 @@
 package setup
 
 import (
+	"openhue-cli/openhue"
 	"openhue-cli/openhue/test/assert"
 	"testing"
 )
 
 func TestNewCmdConfigure(t *testing.T) {
 
-	cmd := NewCmdConfigure()
+	cmd := NewCmdConfigure(openhue.NewTestIOStreamsDiscard())
 
 	assert.ThatCmdUseIs(t, cmd, "config")
 	assert.ThatCmdGroupIs(t, cmd, "config")
